d0723f285ef2bf2bb241971d18013171518edcaa
singhraghav <raghavsingh31@outlook.com>
2021-06-07 21:22:01 +0530

    composing futures and recovering from future failures

--- src/main/scala/parralelism/FuturesAndPromises.scala
+++ src/main/scala/parralelism/FuturesAndPromises.scala
@@ -1,8 +1,10 @@
 package parralelism
 
+import parralelism.FuturesAndPromises.SocialNetwork.fetchBestFriend
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Random, Success, Try}
 
 object FuturesAndPromises extends App {
 
@@ -20,4 +22,62 @@ object FuturesAndPromises extends App {
 
   Thread.sleep(3000)
 
+  case class Profile(id: String, name: String){
+    def poke(another: Profile) = {
+      println(s"${this.name} poking ${another.name}")
+    }
+  }
+
+  object SocialNetwork {
+    val names = Map(
+      "fb.id.1-zuck" -> "Mark",
+      "fb.id.1-Bill" -> "Bill",
+      "fb.id.1-Dummy" -> "Dummy"
+    )
+
+    val friends = Map(
+      "fb.id.1-zuck" -> "fb.id.1-Bill"
+    )
+
+    val random = new Random()
+    // API
+    def fetchProfile(id: String): Future[Profile] = Future {
+      Thread.sleep(random.nextInt(300))
+      Profile(id, names(id))
+    }
+
+    def fetchBestFriend(profile: Profile): Future[Profile] = Future {
+      Thread.sleep(random.nextInt(400))
+      val bfId = friends(profile.id)
+      Profile(bfId, names(bfId))
+    }
+  }
+
+  // client: mark to poke bill
+//  val mark: Future[Unit] = SocialNetwork.fetchProfile("fb.id.1-zuck").flatMap(markProfile => fetchBestFriend(markProfile).map(bill => markProfile.poke(bill)))
+
+  val functionalComposition: Future[Unit] = for {
+    markProfile <- SocialNetwork.fetchProfile("fb.id.1-zuck")
+    bill <- SocialNetwork.fetchBestFriend(markProfile)
+  } yield markProfile.poke(bill)
+
+  Thread.sleep(1000)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
