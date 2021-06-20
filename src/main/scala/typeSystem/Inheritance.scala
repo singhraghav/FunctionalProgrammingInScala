6e0502dde71bb4e78bd93674fd635378bb8979b2
singhraghav <raghavsingh31@outlook.com>
2021-06-20 18:03:34 +0530

    type leniarization

--- src/main/scala/typeSystem/Inheritance.scala
+++ src/main/scala/typeSystem/Inheritance.scala
@@ -20,7 +20,7 @@ object Inheritance extends App {
   }
 
   //Diamond Inheritance -> Last override is always picked
-  trait Animal{def name: String}
+  trait Animal{def name: String} // expands to extends AnyRef with <Animal> -> <Animal> is implementation of animal
   trait Lion extends Animal {override def name: String = "Lion"}
   trait Tiger extends Animal {override def name: String = "Tiger"}
 
@@ -29,4 +29,50 @@ object Inheritance extends App {
   val mu1 = new Mutant
   println(mu1.name) // This will print Lion because it is the last override on mutant and its name function is used
 
+  //The super problem -> type linearization
+  trait Cold {
+    def print = println("Cold")
+  }
+
+  trait Green extends Cold {
+    override def print: Unit = {
+      println("Green")
+      super.print
+    }
+  }
+
+  trait Blue extends Cold {
+    override def print: Unit = {
+      println("Blue")
+      super.print
+    }
+  }
+
+  class Red {
+    def print = println("Red")
+  }
+
+  class White extends Red with Green with Blue {
+    override def print: Unit = {
+      println("White")
+      super.print
+    }
+  }
+
+  val color = new White
+  println()
+  color.print
+  /*
+  * White, Blue, Green, Cold
+  * Why
+  * cold expands to extends AnyRef with <cold>
+  * green expands to extends AnyRef with <cold> with <green>
+  * blue expands to extends AnyRef with <cold> with <blue>
+  * red expands to extends AnyRef with <red>
+  * white is extends AnyRef with ( Red - AnyRef with <red>) with ( Green - AnyRef with <cold> with <green>) with (Blue - AnyRef with <cold> with <blue>) with <white>
+  * type linearization happens
+  * white extends AnyRef with <red> with <cold> with <green> with <blue> with <white>
+  * super moves left to write
+  * white -> blue -> green -> cold and super ends
+  * */
 }
